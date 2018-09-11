1fd3ebd23b4b2520bc7411d88ff22ec6310fabbd
Nicholas Martin <nicholas.martin89@gmail.com>
2018-09-10 18:37:08 -0700

    added a circleci folder with a yml config file.

---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import react from 'react';
+import Reservations from './components/Reservations';
+import store from './store/store';
+import { Provider } from 'react-redux';
