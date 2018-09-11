55fd165ee43bc12d47a801061643f995cc6bafbb
Nicholas Martin <nicholas.martin89@gmail.com>
2018-09-11 12:17:40 -0700

    Changes to the compornets. working on getting the mock up rendered

---
+++ client/src/components/Reservations.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Reservations extends React.Component {
+  constructor(props) {
+    super(props),
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Make a Reservation</h2>
+        <div id="Calendar">
+          <h1>This area is for the calendar block</h1>
+        </div>
+        <div id="timeSlots">
+          <h1>This area is for the timeSlots block</h1>
+        </div>
+        <div id="peoplePerRes">
+          <h1>This area is for the peoplePerRes block</h1>
+        </div>
+        <div id="findTable">
+          <h1>This area is for the findTable block</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Reservations;
---
+++ client/src/index.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import Reservations from './components/Reservations';
+import store from './store/store';
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <Reservations />
+  </Provider>,
+  document.getElementById('reservations'),
+);
